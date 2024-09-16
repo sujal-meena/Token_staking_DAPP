1271acc1a8ee1a26c01562e1467c3dab06f53b8f
sujal-meena <sujalmeena250122@gmail.com>
2024-09-16 22:45:21 +0530

    minor addition

--- web3/contracts/TokenStaking.sol
+++ web3/contracts/TokenStaking.sol
@@ -49,16 +49,143 @@ contract TokenStacking is Ownable, ReentrancyGuard , Initializable{
     event Stake(address indexed user , uint256 amount);
     event UnStake(address indexed user , uint256 amount);
     event EarlyUnStakeFee(address indexed user , uint256 amount);
-
+    event ClaimReward(address indexed user , uint256 amount);
 
     modifier whenTreasuryHasBalance(uint256 amount){
         require(
-            IERC20(_tokenAddre)
-        )
+            IERC20(_tokenAddress).balanceOf(address(this)) >= amount,
+            "Insufficient funds!!"
+        );
+        _;
+    }
+
+    function initialize(
+        address owner_,
+        address tokenAddress_,
+        uint256 apyRate_,
+        uint256 minimumStakingAmount_,
+        uint256 maxStakeTokenLimit_,
+        uint256 stakeStartDate_,
+        uint256 stakeEndDate_,
+        uint256 stakeDays_,
+        uint256 earlyUnstakeFeePercentage_
+    )public virtual initializer{
+        _TokenStaking_init_unchained(
+            owner_,
+            tokenAddress_,
+            apyRate_,
+            minimumStakingAmount_,
+            maxStakeTokenLimit_,
+            stakeStartDate_,
+            stakeEndDate_,
+            stakeDays_,
+            earlyUnstakeFeePercentage_
+        );
+    }
+
+    function __TokenStaking_init_unchained(
+        address owner_,
+        address tokenAddress_,
+        uint256 apyRate_,
+        uint256 minimumStakingAmount_,
+        uint256 maxStakeTokenLimit_,
+        uint256 stakeStartDate_,
+        uint256 stakeEndDate_,
+        uint256 stakeDays_,
+        uint256 earlyUnstakeFeePercentage_
+    )internal onlyInitializing {
+        require(_apyRate <= 10000,"apy rate should be less than 10000");
+        require(stakeDays_ > 0,"Staking days must be non zero");
+        require(tokenAddress_ != address(0),"Token address cannot be a 0 address");
+        require(stakeStartDate_ < stakeEndDate_ ,"Start date must be less than end date");
+
+
+        _transferOwnership(owner_);
+        
+        _tokenAddress = tokenAddress_;
+        _apyRate = apyRate_;
+        _minimumStakingAmount = minimumStakingAmount_;
+        _stakeStartDate = stakeStartDate_;
+        _maxStakeTokenLimit = maxStakeTokenLimit_;
+        _stakeEndDate = stakeEndDate_;
+        _stakeDays = stakeDays_ * 1 days;
+        _earlyUnstakeFeePercentage = earlyUnstakeFeePercentage_;
+
+    }
+
+    //View Methods Start
+
+    /**
+    Function to get minimum staking amount
+     */
+
+    function getMinimumStakingAmount() external view returns (uint256){
+        return _minimumStakingAmount;
+        
+    }
+
+    function getMaxStakingTokenLimit() external view returns (uint256){
+
+        return _maxStakeTokenLimit;
+    }
+
+    function getStakeStartDate() external view returns (uint256){
+        return _stakeStartDate;
+
+}
+    function getStakeEndDate() external view returns (uint256){
+        return _stakeEndDate;
+}
+
+
+    function getTotalStakedTokens() external view returns (uint256){
+        return _totalStakedTokens;
+}
+
+    function getTotalUsers() external view returns (uint256){
+        return _totalUsers;
+}
+
+    function getStakeDays() external view returns (uint256){
+        return _stakeDays;
+}
+    function getEarlyUnstakingFeePercentage() external view returns (uint256){ 
+        return _earlyUnstakeFeePercentage;
+        
+    }
+
+    function getStakingStatus() external view returns (uint256){ 
+        return _isStakingPaused;
+        
+    }
+
+    function getAPY() external view returns (uint256){ 
+        return _apyRate;
+        
+    }
+
+    function getUserEstimatedRewards() external view returns (uint256){ 
+        (uint256 amount , ) = _getUserEstimatedRewards(msg.sender);
+        return _users(msg.sender).rewardAmount + amount;
+        
+    }
+    function getWithdrawableAmount() external view returns (uint256){ 
+        return IERC20(_tokenAddress).balanceOf(address(this)) - _totalStakedTokens;
+        
     }
 
 
+    function getUser(address userAddresss) external view returns (User memory){
+        return _users(userAddresss);
+    }
 
+
+    function isStakeHolder(address _user) external view returns (bool){
+        returns _users[_user].stakeAmount != 0;
+    }
+
+
+    
 
 
 }
